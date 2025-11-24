7c99aa821ba81870e85c942f2f37358a9e5ff5f7
Hushng Fikrat Muhibullah <fikrat86@gmail.com>
2025-11-24 15:39:08 -0500

    App files fixed

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-o	from flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
   return 'Hello from Muhibullah ECS Container'
 
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
 
     
     
